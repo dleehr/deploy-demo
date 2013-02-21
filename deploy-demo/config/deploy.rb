1a9137c9652f9f61762192c33c18a7fab9183885
Dan Leehr <dan.leehr@nescent.org>
2013-02-21 14:42:49 -0500

    Modify deploy config

--- deploy-demo/config/deploy.rb
+++ deploy-demo/config/deploy.rb
@@ -1,18 +1,18 @@
-set :application, "Deploy Demo"
+require 'bundler/capistrano'
 set :repository, "git@github.com:dleehr/deploy-demo.git"  # Your clone URL
 set :scm, "git"
 set :user, "deployer"  # The server's user for deploys
-set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
+set :application, "deploy-demo"
+set :deploy_to, "/home/deployer"
 set :branch, "master"
 set :deploy_via, :remote_cache
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "ubuntu-vm"                          # Your HTTP server, Apache/etc
+role :app, "ubuntu-vm"                          # This may be the same as your `Web` server
+role :db,  "ubuntu-vm", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
