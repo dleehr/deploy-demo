18d2082f8efeb59fab6bffd1a18236aba1bf913c
Dan Leehr <dan.leehr@nescent.org>
2013-02-21 14:53:43 -0500

    Re-add moved files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployDemo::Application.config.secret_token = '48328dc4d14879e253cc61f24ea728b681f9bb411284d920eb84c6a2ebac20ba45d57e2db07042f06882d6a57c48dfd49e82e0bd4908a5a6c9cae61f086347b7'
